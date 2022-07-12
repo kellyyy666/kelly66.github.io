3898eac301ab07b04a14a58eab896d15ff7e3684
kellyyy666 <107507462+kellyyy666@users.noreply.github.com>
2022-07-12 16:01:01 -0400

    Add files via upload

---
+++ 01_messageHistory/sketch.js
@@ -0,0 +1,89 @@
+
+
+let dataServer;
+let pubKey = "pub-c-f7e4ffd6-e9bc-4c70-9fb8-4a120d50d91e";
+let subKey = "sub-c-1d418a72-3059-49cd-81cb-f2be471ebb2c";
+let secretKey = "sec-c-MjA3NzE3NWUtZDEyMC00NzY4LTg3MGQtZTE4NGY1NjIwN2I4";
+
+let channelName = "messageHistory";
+
+let you;
+
+function preload() { 
+
+  // logic to create a random UUID
+    you = random(0,1000000); 
+    console.log(you);
+    you = int(you);
+    console.log(you);
+    you = you.toString();
+  
+}
+
+
+function setup() {
+
+    createCanvas(windowWidth, windowHeight);
+
+    dataServer = new PubNub({
+      subscribeKey: subKey,
+      publishKey: pubKey,
+      uuid: you,
+      secretKey: secretKey,
+      heartbeatInterval: 0,
+    });
+
+     // listen for messages coming through the subcription feed on this specific channel. 
+
+    dataServer.subscribe({ channels: [channelName] });
+    dataServer.addListener({ message: readIncoming });
+   
+  
+    textAlign(CENTER);
+
+    sendTheMessage();
+    fetchMessages();
+
+}
+
+function fetchMessages() {
+console.log("fetching");
+
+  dataServer.fetchMessages(
+    {
+        channels: [channelName],
+        end: '15343325004275466',
+        count: 100
+    },
+    (status, response) => {
+      drawMessages(response.channels.messageHistory);
+    }
+  );
+   
+}
+
+function drawMessages(messageHistory) {
+
+  console.log("in draw messages");
+  background(255);
+  textSize(80);
+  text(messageHistory.length + " messages retrieved", windowWidth/2 , windowHeight/2);
+
+}
+  // PubNub logic below
+function sendTheMessage() {
+  // Send Data to the server to draw it in all other canvases
+  dataServer.publish({
+    channel: channelName,
+    message: {
+      messageText: "Entering the page"
+    },
+  });
+
+}
+
+function readIncoming(inMessage) {
+  console.log(inMessage);
+  fetchMessages();
+
+}
